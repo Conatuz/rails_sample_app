956919baccf63a6703ff716ff89d12f8ae333b07
Thomas McLachlan <creoinvenio@gmail.com>
2013-11-19 19:41:51 +0000

    Improve tests: shared examples, click links, application helper

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,11 @@
-def full_title(page_title)
-  base_title = "Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+# Included by ApplicationHelper now
+#def full_title(page_title)
+#  base_title = "Sample App"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{base_title} | #{page_title}"
+#  end
+#end
