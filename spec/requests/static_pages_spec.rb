06ccf6409e3b818bca78a68e4d03579d47780417
Thomas McLachlan <creoinvenio@gmail.com>
2013-11-19 18:40:35 +0000

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,17 @@ require 'spec_helper'
 
 describe "Static Pages" do
   
-  let(:base_title) { "Sample App" }
+  #let(:base_title) { "Sample App" }
   subject { page } # page.should -> should
   
   describe "Home Page" do
     before { visit root_path }  # eliminates need to visit page each time
     
     it { should have_selector('h1', text: "Sample App") }
-    it { should have_selector('title', text: "#{base_title}") }
+    it { should have_selector('title', text: full_title('') ) }
     it { should_not have_selector('title', text: " | Home") }
+    
+    # full_title() defined in /spec/support/utilities.rb
     
     #it "should have the h1 'Sample App'" do
     #  visit root_path
@@ -33,7 +35,7 @@ describe "Static Pages" do
     before { visit help_path }
     
     it { should have_selector('h1', text: "Help") }
-    it { should have_selector('title', text: "#{base_title} | Help") }
+    it { should have_selector('title', text: full_title('Help') ) }
     
   end
   
@@ -41,7 +43,7 @@ describe "Static Pages" do
     before { visit about_path }
     
     it { should have_selector('h1', text: "About Us") }
-    it { should have_selector('title', text: "#{base_title} | About Us") }
+    it { should have_selector('title', text: full_title('About Us') ) }
     
   end
   
@@ -49,7 +51,7 @@ describe "Static Pages" do
     before { visit contact_path }
     
     it { should have_selector('h1', text: "Contact") }
-    it { should have_selector('title', text: "#{base_title} | Contact") }
+    it { should have_selector('title', text: full_title('Contact') ) }
     
   end
   
