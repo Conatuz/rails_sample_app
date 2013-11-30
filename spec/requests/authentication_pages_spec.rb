e0ea77eab734a3e3931a218ce8e0a220b18aa80c
Thomas McLachlan <creoinvenio@gmail.com>
2013-11-30 23:22:24 +0000

    Start micropost manipulation features

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -75,6 +75,20 @@ describe "Authentication" do
         
       end # end of Users controller
       
+      describe "in the Microposts controller" do
+        
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+      end  # end of Microposts controller
+      
       describe "when attempting to visit a protected page" do
         
         before do
